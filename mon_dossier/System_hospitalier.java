23f030093d0c49c99f6e226b54564d911aea07a0
Mamessier <mattmamessier@gmail.com>
2021-03-16 11:01:51 +0100

    j'ai modifié les methodes afin de les commenter

---
+++ mon_dossier/System_hospitalier.java
@@ -0,0 +1,46 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+	
+	private List<Requête> listRequetes;
+
+	public System_hospitalier(List<Requête> listRequetes) {
+		super();
+		this.listRequetes = listRequetes;
+	}
+
+	public System_hospitalier() {
+		super();
+		listRequetes = new ArrayList<>();
+	}
+
+	public List<Requête> getListRequetes() {
+		return listRequetes;
+	}
+
+	public void setListRequetes(List<Requête> listRequetes) {
+		this.listRequetes = listRequetes;
+	}
+	
+	public void recevoirRequete(Requête requete) {
+		listRequetes.add(requete);
+		String analyse =  analyserRequete(requete);
+		String comparaisonDonnees = comparerDonnes();
+		System.out.println(analyse);
+		System.out.println(comparaisonDonnees);
+	}
+	
+	private String analyserRequete(Requête requête) {
+		// cette methode prend la requete en parametre et l'analyse pour vérifier qu'elle existe et ce qu'elle fait
+		return "requête analysée";
+	}
+	
+	private String comparerDonnes() {
+		// cette methode permet de comparer toutes les données afin de verifier s'il y a un cluster qu'il signale dans ce cas
+		return "Cluster trouvé";
+	}
+
+}
